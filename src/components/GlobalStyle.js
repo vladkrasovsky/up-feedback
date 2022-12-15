11baafde5a4530f0e781ae63281bbf0017704720
Vladyslav Krasovsky <v.o.krasovsky@gmail.com>
2022-12-15 09:36:00 +0200

    Update GlobalStyle.js

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -10,6 +10,7 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Arsenal', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    background-color: #382E2D;
   }
 
   ::placeholder {
